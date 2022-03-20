8e8e5881063765e057fe28dc4a460dc11113cf7d
saulrazo <saulrazom32@gmail.com>
2022-03-20 15:43:32 -0600

    1

---
+++ semana05_vue/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data(){
+      return{
+            nombre: 'Saul',
+            apellido: 'Razo',
+            email: 'saulrazo900@gmail.com',
+            genero: 'male',
+            foto: 'https://randomuser.me/api/portraits/men/10.jpg',
+            }
+          },
+
+         
+
+        }).mount('#app')
